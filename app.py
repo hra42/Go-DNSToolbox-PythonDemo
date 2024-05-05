9a4055600dc4aa1b227cdc3d14e7624495614ecd
Henry Rausch <44985923+HRA42@users.noreply.github.com>
2024-05-05 19:47:26 +0200

    added some docs for the user

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ DNS_SERVERS = {
     "Quad9": "9.9.9.9"
 }
 
+st.set_page_config(page_title="DNS-Toolbox", page_icon="🤖", layout="wide")
+
 #MARK: - DNS Function
 def get_dns_record(domain, dns_server, type):
     resolver = dns.resolver.Resolver()
@@ -28,15 +30,25 @@ def get_dns_record(domain, dns_server, type):
 def intro():
     st.write("# Welcome to the Demo of DNS-Toolbox")
 
+    st.warning("This is a demo version of DNS-Toolbox. This is subject to change. The real application is not yet ready for prime time and will be delivered instead of this one in a few months time.")
+    
     st.markdown(
     """
     This is a simple web application that demonstrates the use of DNS-Toolbox.
     The App is designed to allow for DNS queries to be made like MXToolbox.com.
     It can help you solve DNS problems, find out about your DNS records, and more.
     Another Demo will focus around the check of SSL certificates.
-
-    You can find a desktop version of this application on my GitHub page:
-    [DNS-Toolbox](https://github.com/hra42/Go-DNS)
+    
+    For an broad overview use the M365 Check, if you are using Microsoft 365.
+    
+    The SSL Check will help you to check the SSL certificate of a webpage.
+    
+    Explanation of the DNS Records:
+    - A and AAAA Records point you to an IP Address (mostly used for web pages)
+    - CNAME Records point you to another domain (mostly used for web pages hosted by another service like GitHub Pages)
+    - MX Records point you to a mail exchange server (used for mail servers)
+    - NS Records point you to a nameserver (mostly used for mail servers, here you can find out who to ask for changes in the domain configuration, i.e. for DKIM configuration)
+    - TXT Records point you to a text record (used for SPF and other authentication purposes)
     """
 )
 
