3b8e1069ac722bb2392a027a211fcb82d6edcac0
Henry Rausch <44985923+HRA42@users.noreply.github.com>
2024-05-15 15:45:31 +0200

    Refactored intro text and improved formatting
    The introductory text in the web application has been refactored for clarity and brevity. The changes include a more concise description of the app's purpose, a new 'Features' section highlighting key functionalities, and an enhanced explanation of DNS records with better formatting for readability.

--- internal/intro.py
+++ internal/intro.py
@@ -8,20 +8,19 @@ def get_intro():
     
     st.markdown(
     """
-    This is a simple web application that demonstrates the use of DNS-Toolbox.
-    The App is designed to allow for DNS queries to be made like MXToolbox.com.
-    It can help you solve DNS problems, find out about your DNS records, and more.
-    Another Demo will focus around the check of SSL certificates.
-    
-    For an broad overview use the M365 Check, if you are using Microsoft 365.
-    
-    The SSL Check will help you to check the SSL certificate of a webpage.
-    
-    Explanation of the DNS Records:
-    - A and AAAA Records point you to an IP Address (mostly used for web pages)
-    - CNAME Records point you to another domain (mostly used for web pages hosted by another service like GitHub Pages)
-    - MX Records point you to a mail exchange server (used for mail servers)
-    - NS Records point you to a nameserver (mostly used for mail servers, here you can find out who to ask for changes in the domain configuration, i.e. for DKIM configuration)
-    - TXT Records point you to a text record (used for SPF and other authentication purposes)
+    This is a simple web application that demonstrates the use of DNS-Toolbox. The app is designed to allow for DNS queries similar to MXToolbox.com. It can help you solve DNS problems, find out about your DNS records, and more. Another demo will focus on checking SSL certificates.
+
+    ### Features
+
+    - **M365 Check**: For a broad overview if you are using Microsoft 365.
+    - **SSL Check**: Helps you check the SSL certificate of a webpage.
+
+    ### Explanation of DNS Records
+
+    - **A and AAAA Records**: Point to an IP address (mostly used for web pages).
+    - **CNAME Records**: Point to another domain (mostly used for web pages hosted by another service like GitHub Pages).
+    - **MX Records**: Point to a mail exchange server (used for mail servers).
+    - **NS Records**: Point to a nameserver (mostly used for mail servers, useful for finding out who to ask for changes in the domain configuration, e.g., for DKIM configuration).
+    - **TXT Records**: Point to a text record (used for SPF and other authentication purposes).
     """
 )
